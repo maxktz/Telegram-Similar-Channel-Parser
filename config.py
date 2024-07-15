5e37b57bab4a9f2fced403e5bacc765f35b0ab43
zrxmax <zrxmax@icloud.com>
2024-07-15 19:15:18 +0300

    format file

--- config.py
+++ config.py
@@ -2,10 +2,10 @@
 PROXY = None
 
 # code parameters (don't touch if you don't know what you're doing)
-LINE_FORMAT = '{username}:{participants_count}:{title}'
-SAVING_DIRECTORY = 'saved_channels'
+LINE_FORMAT = "{username}:{participants_count}:{title}"
+SAVING_DIRECTORY = "saved_channels"
 TELEGRAM_API_ID = 2040
-TELEGRAM_API_HASH = 'b18441a1ff607e10a989891a5462e627'
-TELEGRAM_DEVICE_MODEL = 'MacBook Air M1'
-TELEGRAM_SYSTEM_VERSION = 'macOS 14.4.1'
-TELEGRAM_APP_VERSION = '4.16.8 arm64'
+TELEGRAM_API_HASH = "b18441a1ff607e10a989891a5462e627"
+TELEGRAM_DEVICE_MODEL = "MacBook Air M1"
+TELEGRAM_SYSTEM_VERSION = "macOS 14.4.1"
+TELEGRAM_APP_VERSION = "4.16.8 arm64"
